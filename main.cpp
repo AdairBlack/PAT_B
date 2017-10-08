97ba148aed9d55012f31fa43114d078dbd7d7dbd
NightsWatch <972947350@qq.com>
2017-10-08 22:37:55 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstring>
+int main(){
+    int sum = 0;
+    char str[110];
+    scanf("%s", str);
+    int len = strlen(str);
+    for(int i = 0; i < len; i++){
+        
+    }
+}
