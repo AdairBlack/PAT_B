426ad7d93180b09a2afea3935d2722cb27a268fb
AdairBlack <972947350@qq.com>
2020-08-28 07:43:06 +0800

    B1004

--- B1004.cpp
+++ B1004.cpp
@@ -1,30 +1,42 @@
+// PatTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+
+*/
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
 #include <cstdio>
 #include <cstring>
 
-struct Student{
-    char name[15];
-    char id[15];
-    int score;
-}max, min, temp;
+typedef struct StudentInfo{
+	char name[15];
+	char id[15];
+	int score;
+}Student;
 
-int main(){
-    max.score = 0;
-    min.score = 110;
-    int n;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-        scanf("%s %s %d", temp.name, temp.id, &temp.score);
-        if(temp.score > max.score){
-            strcpy(max.name, temp.name);
-            strcpy(max.id, temp.id);
-            max.score = temp.score;
-        }
-        if(temp.score < min.score){
-            strcpy(min.name, temp.name);
-            strcpy(min.id, temp.id);
-            min.score = temp.score;
-        }
-    }
-    printf("%s %s\n", max.name, max.id);
-    printf("%s %s\n", min.name, min.id);
+int main()
+{
+	int n = 0;
+	Student high, low, temp;
+	high.score = 0;
+	low.score = 101;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%s %s %d", temp.name, temp.id, &temp.score);
+		if (temp.score > high.score) {
+			strcpy(high.name, temp.name);
+			strcpy(high.id, temp.id);
+			high.score = temp.score;
+		}
+		if (temp.score < low.score) {
+			strcpy(low.name, temp.name);
+			strcpy(low.id, temp.id);
+			low.score = temp.score;
+		}
+	}
+	printf("%s %s\n", high.name, high.id);
+	printf("%s %s\n", low.name, low.id);
+
+
+	return 0;
 }
