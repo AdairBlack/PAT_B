65fd3ae404f5e4364fadd54078ee74807c841faf
AdairBlack <972947350@qq.com>
2020-09-24 07:47:39 +0800

    1006.1007

--- B1006.cpp
+++ B1006.cpp
@@ -1,21 +1,30 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
 
-int main(){
-    int num;
-    scanf("%d", &num);
-    int x = 0, y = 0, z = 0;
-    x = num / 100;
-    y = num % 100 / 10;
-    z = num % 10;
-    for(int i = 0; i < x; i++){
-        printf("B");
-    }
-    for(int i = 0; i < y; i++){
-        printf("S");
-    }
-    for(int i = 1; i <= z; i++){
-        printf("%d", i);
-    }
-    printf("\n");
-    return 0;
+using namespace std;
+
+int main()
+{
+	int n = 0, b = 0, s = 0;
+	scanf("%d", &n);
+	b = n / 100;
+	n %= 100;
+	s = n / 10;
+	n %= 10;
+	for (int i = 0; i < b; i++) {
+		printf("%c", 'B');
+	}
+	for (int i = 0; i < s; i++) {
+		printf("%c", 'S');
+	}
+	for (int i = 1; i <= n; i++) {
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return 0;
 }
+
--- B1007.cpp
+++ B1007.cpp
@@ -1,40 +1,8 @@
 #include <cstdio>
 
-bool flag[100010] = {false};
-int prime[100010];
-int count = 0;
-
-void init(int n){
-    for(int i = 2; i <= n; i++){
-        flag[i] = true;
-    }
-    for(int i = 2; i <= n; i++){
-        if(!flag[i]) continue;
-        int temp = i;
-        while(temp < n){
-            if((temp + i) <= n){
-                flag[temp + i] = false;
-            }
-            temp += i;
-        }
-    }
-    for(int i = 2; i <= n; i++){
-        if(flag[i] == true){
-            prime[count++] = i;
-        }
-    }
-}
-
-
-int main(){
-    int n;
-    scanf("%d", &n);
-    init(n);
-    int ans = 0;
-    for(int i = 0; i < count - 1; i++){
-        if(prime[i + 1] - prime[i] == 2){
-            ans++;
-        }
-    }
-    printf("%d\n", ans);
-}
+int main()
+{
+    printf("Hello World!\n");
+    int i = 1;
+    return 0;
+} 
