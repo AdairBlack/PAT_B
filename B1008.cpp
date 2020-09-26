fc0ac9dc04d2e791e94bdf137a1399dcbcb75ff7
AdairBlack <972947350@qq.com>
2020-09-27 07:38:36 +0800

    B1008

--- B1008.cpp
+++ B1008.cpp
@@ -1,37 +1,29 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
 
-int gcd(int a, int b){
-    if(b == 0) return a;
-    else return (b, a % b);
+using namespace std;
+
+int main()
+{
+	int n = 0, m = 0, temp = 0;
+	int a[110] = { 0 };
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) 
+	{
+		scanf("%d", &temp);
+		a[(i + m) % n] = temp;
+	}
+
+	printf("%d", a[0]);
+	for (int i = 1; i < n; i++)
+	{
+		printf(" %d", a[i]);
+	}
+
+	return 0;
 }
 
-int main(){
-    int n, m;
-    int num[110];
-    scanf("%d%d", &n, &m);
-    for(int i = 0; i < n; i++){
-        scanf("%d", &num[i]);
-    }
-    m %= n;
-    if(m != 0){
-        int cycle = gcd(m, n);
-        for(int i = n - m; i < n - m + cycle; i++){
-            int temp = num[i];
-            int pos = i;
-            do{
-                int next = (pos - m + n) % n;
-                if(next != i){
-                    num[pos] = num[next];
-                } else {
-                    num[pos] = temp;
-                }
-                pos = next;
-            }while(pos != i);
-        }
-    }
-    for(int i = 0; i < n; i++){
-        printf("%d",num[i]);
-        if(i != (n - 1)) printf(" ");
-    }
-    return 0;
-}
